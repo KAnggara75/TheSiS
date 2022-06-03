f266f3e681d04857b4b235a755058a96b0416b28
Call Vin <kanggara75@gmail.com>
2022-06-04 05:10:44 +0700

    test: deploy #web

--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -9,25 +9,25 @@
 </head>
 
 <body class="antialiased">
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+  <div class="items-top relative flex min-h-screen justify-center bg-gray-100 py-4 dark:bg-gray-900 sm:items-center sm:pt-0">
     @if (Route::has('login'))
-      <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+      <div class="fixed top-0 right-0 hidden px-6 py-4 sm:block">
         @auth
-          <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+          <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline dark:text-gray-500">Home</a>
         @else
-          <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+          <a href="{{ route('login') }}" class="text-sm text-gray-700 underline dark:text-gray-500">Log in</a>
           @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline dark:text-gray-500">Register</a>
           @endif
         @endauth
       </div>
     @endif
 
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-6xl sm:px-6 lg:px-8">
 
-      <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-        <div class="ml-4 text-center text-2xl text-sky-500 sm:text-right sm:ml-0">
-          Laravel v{{ Illuminate\Foundation\Application::VERSION }} x
+      <div class="mt-4 flex justify-center sm:items-center sm:justify-between">
+        <div class="ml-4 text-center text-2xl text-sky-500 sm:ml-0 sm:text-right">
+          TheSiS Web with Laravel v{{ Illuminate\Foundation\Application::VERSION }} x
           Tailwind CSS
           <?php
           $packageJSON = json_decode(file_get_contents(base_path() . '/package.json'));
